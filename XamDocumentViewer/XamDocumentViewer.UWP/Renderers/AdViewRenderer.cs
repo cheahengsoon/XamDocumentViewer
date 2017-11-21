6e1aec0bcc7b2bb68e5282731333db738cef9821
Tushar Koshti <koshtitushar1994@gmail.com>
2017-11-21 22:26:13 +0530

    Added ads in UWP project and updated nuget packages.

--- XamDocumentViewer/XamDocumentViewer.UWP/Renderers/AdViewRenderer.cs
+++ XamDocumentViewer/XamDocumentViewer.UWP/Renderers/AdViewRenderer.cs
@@ -1,48 +1,50 @@
-﻿using Xamarin.Forms.Platform.UWP;
+﻿using Microsoft.Advertising.WinRT.UI;
+using Windows.System.Profile;
+using Xamarin.Forms.Platform.UWP;
 using XamDocumentViewer.Droid.Renderers;
 using XamDocumentViewer.Standard.Controls;
 
 [assembly: ExportRenderer(typeof(AdControlView), typeof(AdViewRenderer))]
 namespace XamDocumentViewer.Droid.Renderers
 {
-	public class AdViewRenderer : ViewRenderer<AdControlView, FormsTextBox>
+	public class AdViewRenderer : ViewRenderer<AdControlView, AdControl>
 	{
 		string bannerId = "1100001579";
-		//AdControl adView;
+		AdControl adView;
 		string applicationID = "9pb4vjp3k60p";
-		//void CreateNativeAdControl()
-		//{
-		//	if (adView != null)
-		//		return;
+		void CreateNativeAdControl()
+		{
+			if (adView != null)
+				return;
 
-		//	var width = 300;
-		//	var height = 50;
-		//	if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Desktop")
-		//	{
-		//		width = 728;
-		//		height = 90;
-		//	}
-		//	// Setup your BannerView, review AdSizeCons class for more Ad sizes. 
-		//	adView = new AdControl
-		//	{
-		//		ApplicationId = applicationID,
-		//		AdUnitId = bannerId,
-		//		HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center,
-		//		VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Bottom,
-		//		Height = height,
-		//		Width = width
-		//	};
+			var width = 300;
+			var height = 50;
+			if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Desktop")
+			{
+				width = 728;
+				height = 90;
+			}
+			// Setup your BannerView, review AdSizeCons class for more Ad sizes. 
+			adView = new AdControl
+			{
+				ApplicationId = applicationID,
+				AdUnitId = bannerId,
+				HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center,
+				VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Bottom,
+				Height = height,
+				Width = width
+			};
 
-		//}
+		}
 
-		//protected override void OnElementChanged(ElementChangedEventArgs<AdControlView> e)
-		//{
-		//	base.OnElementChanged(e);
-		//	if (Control == null)
-		//	{
-		//		CreateNativeAdControl();
-		//		SetNativeControl(adView);
-		//	}
-		//}
+		protected override void OnElementChanged(ElementChangedEventArgs<AdControlView> e)
+		{
+			base.OnElementChanged(e);
+			if (Control == null)
+			{
+				CreateNativeAdControl();
+				SetNativeControl(adView);
+			}
+		}
 	}
 }
